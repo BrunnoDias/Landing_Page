b4107e2b5f9525d2d6d8a3dbdf2110e55c241c07
bruno <bruno.dias@applus.com>
2023-02-10 21:06:06 -0300

    função mudar para modo dark

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function modoDark() {
+    var tema = document.getElementsByTagName('link')[0];
+
+    if (tema.getAttribute('href') == './css/style.css') {
+        tema.setAttribute('href', './css/style_dark.css');
+    } else {
+        tema.setAttribute('href', './css/style.css');
+    }
+}
